2d85b9e9fad288d7f2ec8c8162c121eed247474d
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2023-04-12 19:48:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaração de variáveis
+            int b;
+            int h;
+            int a;
+            // Entrada dos dados
+            Console.Write("Informe a base: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Informe a altura: ");
+            h = int.Parse(Console.ReadLine());
+            // Operação aritmética
+            a = b * h;
+
+            // Console.Write("O valor da área é ");
+            // Console.WriteLine(a);
+
+            // Exibição do resultado
+            Console.WriteLine("O valor da área de um retângulo que tem base {0} e altura {1} é {2}", b, h, a);
+
+
+        }
+    }
+}
